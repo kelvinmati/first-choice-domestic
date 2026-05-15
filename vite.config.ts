8a6ad0844d2fa7704db64baedc246c6033234ab0
kelvin mati <kelvinmati45@gmail.com>
2026-05-15 16:35:14 +0300

    bug fix 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,29 @@
+// import { reactRouter } from "@react-router/dev/vite";
+// import tailwindcss from "@tailwindcss/vite";
+// import { defineConfig } from "vite";
+// import { fileURLToPath, URL } from "node:url";
+
+// export default defineConfig({
+//   base: "/",
+//   plugins: [tailwindcss(), reactRouter()],
+//   resolve: {
+//     alias: {
+//       "~": fileURLToPath(new URL("./app", import.meta.url)),
+//     },
+//   },
+// });
+
+
 import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
-import { fileURLToPath, URL } from "node:url";
+import tsconfigPaths from "vite-tsconfig-paths";
+// ↓ add this
+import netlifyReactRouter from "@netlify/vite-plugin-react-router";
 
 export default defineConfig({
-  base: "/",
-  plugins: [tailwindcss(), reactRouter()],
-  resolve: {
-    alias: {
-      "~": fileURLToPath(new URL("./app", import.meta.url)),
-    },
-  },
+  plugins: [
+    reactRouter(),
+    tsconfigPaths(),
+    netlifyReactRouter({ edge: true }) // ← add this
+  ]
 });
