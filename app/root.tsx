4465f1da6c9f2a0438fbff227a9fb38b4e807226
kelvin mati <kelvinmati45@gmail.com>
2026-05-15 09:22:58 +0300

    bug fix

--- app/root.tsx
+++ app/root.tsx
@@ -11,7 +11,7 @@ export default function App() {
       <body>
         <Navbar /> 
         <main>
-          <Outlet />
+        <Outlet />
         </main>
         <Footer />
         <WhatsAppChat />
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,5 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "/", 
   plugins: [tailwindcss(), reactRouter()],
-  resolve: {
-    tsconfigPaths: true,
-  },
+
 });
